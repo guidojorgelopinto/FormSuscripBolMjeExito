72f9563f863ffc5d5651f11e291502a09477df2a
Guido Jorge Lo Pinto <guidojorgelopinto@gmail.com>
2023-10-04 14:59:07 +0100

    3-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/newsletter', pathMatch: 'full', },
+  { path: '', redirectTo: '/newsletter', pathMatch: 'full' },
   { path: 'newsletter', component: NewsletterComponent  },
 ];
 
